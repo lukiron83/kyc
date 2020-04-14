c8464a5e74f3f9aee52460304aa8bd9f31f554c2
Lukasz <lukiron@o2.pl>
2020-04-14 04:57:43 +0200

    Reading - single detail by Strategy - Party - Profile (with details,relations,parties details) Based on Arrays (no H2 usage)

--- src/App.java
+++ src/App.java
@@ -14,8 +14,11 @@ public class App {
 
     private static void testDetails() {
         System.out.println("KYCTest");
+
+        //Setting data - look to the class what is where (I used Arrays instead of H2. All data is on GUIDS no Ids
         DataService.Init();
 
+        //Single party detail (for party identified by GUID) by Facade and sufficient Strategy for PartyDeatails reading and ProfileDetails
         DetailFacade detailFacade = new DetailFacade();
         DetailRequest detailRequest = new DetailRequest(null,UUID.fromString("20000000-0000-0000-0000-0000000000000"),"FIRST_NAME");
         DetailBasic detail = detailFacade.getDetail(detailRequest);
@@ -24,6 +27,7 @@ public class App {
         else
             System.out.println("Detail not set");
 
+        //Single profile detail - party Id is null here
         DetailRequest detailRequest2 = new DetailRequest(UUID.fromString("00000000-0000-0000-0000-0000000000077"),null,"KYCID");
         detail = detailFacade.getDetail(detailRequest2);
         if (detail!=null)
@@ -31,11 +35,41 @@ public class App {
         else
             System.out.println("Detail not set");
 
+        //Reading whole ProfileEntity - using ProfileBuilder
+        //This Director (Creator) creates Entity only with it's attributes
         EntityCreatorBasic creatorBasic= new EntityCreatorAtr();
         ProfileBuilder builderProfile = new ProfileBuilder(UUID.fromString("00000000-0000-0000-0000-0000000000077"));
         creatorBasic.Construct(builderProfile);
         Entity entity = builderProfile.build();
+        entity.show(); //This is shit - use Breakpoint/Watch unil I will do the DetailPrinter
+
+        //Same Profile, same Builder differnt Director - read only Relations
+        creatorBasic= new EntityCreatorRels();
+        builderProfile = new ProfileBuilder(UUID.fromString("00000000-0000-0000-0000-0000000000077"));
+        creatorBasic.Construct(builderProfile);
+        entity = builderProfile.build();
         entity.show();
 
+        //Same Profile - attributes and Relations
+        creatorBasic= new EntityCreatorAtrRels();
+        builderProfile = new ProfileBuilder(UUID.fromString("00000000-0000-0000-0000-0000000000077"));
+        creatorBasic.Construct(builderProfile);
+        entity = builderProfile.build();
+        entity.show();
+
+        //Single PArty read - attributes only
+        creatorBasic= new EntityCreatorAtr();
+        PartyBuilder builderParty = new PartyBuilder(UUID.fromString("20000000-0000-0000-0000-0000000000000"));
+        creatorBasic.Construct(builderParty);
+        entity = builderParty.build();
+        entity.show();
+        
+        //Other Profile - all (attributes, relations, and attributes of parties in relations)
+        //Inside of ProfileBuilder I used PartyBuilder
+        creatorBasic= new EntityCreatorAtrRelsAtr();
+        builderProfile = new ProfileBuilder(UUID.fromString("00000000-0000-0000-0000-0000000000099"));
+        creatorBasic.Construct(builderProfile);
+        entity = builderProfile.build();
+        entity.show();
     }
 }
